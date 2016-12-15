4c9f04975f0885e4c957abe3cba7fe760944c117
lzuo <lzuo@Lzuo.local>
2016-12-15 17:39:18 +0800

    -null

--- src/main/java/com/ebupt/oss/OSSController.java
+++ src/main/java/com/ebupt/oss/OSSController.java
@@ -1,9 +1,8 @@
 package com.ebupt.oss;
 
 import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 
@@ -25,32 +24,35 @@ public class OSSController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/delivery/upload", method = RequestMethod.POST, produces={"application/json;charset=UTF-8"})
-    public UploadBean upload(MultipartFile file) {
-        String uuid = UUID.randomUUID().toString().replace("-", "");
-
-        String url = "https://123.56.26.82:8443/OSS/delivery/download?uuid=" + uuid;
-
-        String prefix = uuid.substring(0, 1);
-        String suffix = uuid.substring(2, 4);
-
+    public UploadBean upload(MultipartFile file, HttpServletRequest request) {
+        logger.info("文件上传访问记录:" + "请求端的url：" + request.getRequestURL().toString());
+        UploadBean uploadBean = new UploadBean("-1", "-1");
         try {
+            String uuid = UUID.randomUUID().toString().replace("-", "");
+
+            String url = "https://123.56.26.82:8443/OSS/delivery/download?uuid=" + uuid;
+
+            String prefix = uuid.substring(0, 1);
+            String suffix = uuid.substring(2, 4);
 
             FileUtils.writeByteArrayToFile(new File("/home/oss/repertory/delivery/" + prefix + "/"
-                    + suffix + "/" + uuid), file.getBytes());
+                        + suffix + "/" + uuid), file.getBytes());
 
             /*
             FileUtils.writeByteArrayToFile(new File("/Users/lzuo/IdeaProjects/oss/src/main/resources/repertory/delivery/" + prefix + "/"
                     + suffix + "/" + fileName), file.getBytes());
             */
+
+            logger.info("本次文件上传产生的uuid为：" + uuid + ", 返回的url为：" + url);
+
+            uploadBean.setUrl(url);
+            uploadBean.setUuid(uuid);
+
         } catch (IOException e) {
-            logger.warn("错误原因：" + e.getCause() + " 错误信息:" + e.getMessage());
+            logger.warn("出错原因:" + e.getCause() + ", 错误信息：" + e.getMessage());
+        } finally {
+            return uploadBean;
         }
-
-        logger.info("文件上传访问记录:" + "产生的uuid为" + uuid + "， 返回的对应文件下载url为：" + url);
-
-        UploadBean uploadBean = new UploadBean(url, uuid);
-
-        return uploadBean;
     }
 
     @RequestMapping(value = "/delivery/download", method = RequestMethod.GET)
@@ -61,23 +63,20 @@ public class OSSController {
         String filePath = "/home/oss/repertory/delivery/" + prefix + "/" + suffix + "/" + uuid;
         File file = new File(filePath);
 
-        if (file.exists()) {
-            response.setHeader("Content-Type", "application/json;charset=UTF-8");
-            response.setHeader("Content-Encoding", "gzip");
-            response.setContentLength((int) file.length());
+        try {
+            if (file.exists()) {
+                response.setHeader("Content-Type", "application/json;charset=UTF-8");
+                response.setHeader("Content-Encoding", "gzip");
+                response.setContentLength((int) file.length());
 
-            try {
                 InputStream fileInputStream = new FileInputStream(filePath);
                 IOUtils.copy(fileInputStream, response.getOutputStream());
-            } catch (IOException e) {
-                logger.warn("错误原因：" + e.getCause() + " 错误信息:" + e.getMessage());
-            }
-        } else {
-            try {
+
+            } else{
                 response.sendError(SC_NOT_FOUND, "NOT FOUND");
-            } catch (IOException e) {
-                logger.warn("错误原因：" + e.getCause() + " 错误信息:" + e.getMessage());
             }
+        } catch (IOException e) {
+            logger.warn("错误原因：" + e.getCause() + " 错误信息:" + e.getMessage());
         }
     }
 
